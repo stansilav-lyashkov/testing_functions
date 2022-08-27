6add1cd623cfb28b1c818b80685ef64673b1cca6
Stanislav Lyashkov <lyashkov.stanislav@mail.ru>
2022-08-27 18:45:01 +0300

    I have written buble_sort function and added tests for it.

---
+++ sorts.py
@@ -0,0 +1,35 @@
+import doctest
+
+
+
+def buble_sort(array):
+    """
+    >>> buble_sort([3,0, 10])
+    [0, 3, 10]
+    >>> buble_sort([])
+    []
+    """
+
+
+
+    sorted_array = array
+    for j in range(len(array)):
+        for i in range(len(array)-1):
+            if sorted_array[i] > sorted_array[i+1]:
+                sorted_array[i], sorted_array[i+1] = sorted_array[i+1], sorted_array[i]
+
+    return sorted_array
+
+
+
+
+
+
+def helo():
+    """
+    >>> helo()
+    Hello!
+    """
+    print("Hello!")
+
+doctest.testmod()
